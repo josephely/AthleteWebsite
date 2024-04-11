74d523199703c22a7622cdbbc97a3cfbb98aaf95
Joseph Ely <joseph@solzorro.com>
2024-04-11 16:55:40 -0600

    sup boys

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Dis is my app brotha</h1>
+        <h1>Basketball Player Index</h1>
       </header>
+      <h2>Search for an NBA player</h2>
+      <MyForm></MyForm>
     </div>
   );
+}
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
 }
 
 export default App;
